76838239e1314611e33ec5121f8434fbc97ccb09
Pritish Khera <pritish.khera@gmail.com>
2022-07-25 21:47:16 +0530

    commit

---
+++ PointerToStructure.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+struct rectangle{
+        int length;
+        int breadth;
+    };
+
+int main(){
+
+     rectangle*p;
+     p=new rectangle;
+     p->length=15;
+     p->breadth=7;
+
+     
+
+    
+    
+    return 0;
+
+}
