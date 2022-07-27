7d264662c49a9f57adf6d21ea1671c24515730c6
Pritish Khera <pritish.khera@gmail.com>
2022-07-27 22:13:58 +0530

    commit

---
+++ Functions.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+int add(int a,int b){
+    int c;
+    c=a+b;
+    return c;
+}
+
+
+int main(){
+    cout<<add(2,5);
+
+    return 0;
+}
---
+++ ParameterPassing.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void swap(int *x,int *y){
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+     
+
+}
+
+
+
+int main(){
+
+    
+
+
+
+
+    int a=5;
+    int b=7;
+    swap(&a,&b);
+    cout<<"a is"<<a<<endl;
+    cout<<"b is"<<b<<endl;
+
+
+
+ }
