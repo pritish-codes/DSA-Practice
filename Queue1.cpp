0efd7e2fbc198afd74c53066c86da41f96243edd
Pritish Khera <pritish.khera@gmail.com>
2022-08-28 13:35:33 +0530

    commit

---
+++ Queue1.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+#define SIZE 10
+
+void enQueue(int);
+void deQueue();
+void display();
+
+int queue[SIZE],front=-1,rear=-1;
+
+
+
+int main(){
+
+    int value,choice;
+    
+
+    
+
+
+}
---
+++ TowerOfHanoi.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void towers(int,char,char,char);
+
+void main(){
+    int num;
+    printf("Enter the number of disks ");
+    scanf("%d",&num);
+    printf("")
+
+}
